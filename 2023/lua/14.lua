97ce585054597dff1564915a9bf016d2a04d3303
Skyler Kehren <pyrodogg@gmail.com>
2023-12-14 23:51:59 +0200

    Day 14 with caching, smh

--- 2023/lua/14.lua
+++ 2023/lua/14.lua
@@ -1,6 +1,7 @@
 package.path = package.path .. ';../../?.lua'
 require "util"
 local inspect = require "inspect"
+local md5 = require "md5"
 
 local lines = lines_from(arg[1] or ('../input/'..string.gsub(arg[0],'lua','txt')))
 local P1, P2 = 0, 0
@@ -29,71 +30,27 @@ local function mapstring(m)
     return s
 end
 
-
--- for k,v in pairs(lines) do
---     map[k] = {}
---     for i=1, 100 do 
---         local char = v:sub(i,i)
---         if char == '#' or char == 'O' then
---             if k == 1  or char == "#" then
---                 -- first row and # are stuck in place
---                 --print('adding at ', k, i, v:sub(i,i))
---                 map[k][i] = v:sub(i,i)
---                 -- if char == "O" then
---                 --     P1 = P1 + 100
---                 -- end
---             else
---                 local floor = 0
---                 for j=k-1, 1, -1 do 
---                     if map[j][i] ~= nil then
---                         floor = j
---                         break
---                     end
---                 end
---                 map[floor+1][i] = v:sub(i,i)
---                 --P1 = P1 + (100-(floor+1)+1)
---             end
---         end
---     end
-
---     --if k >= 10 then break end
--- end
-
-
 local function collapseNorthSouth(dir)
     local step = (dir == "N" and 1) or -1
 
     for col = 1, width do
-
         local lookstart = (dir == "N" and 1) or height
         local lookstop = (dir == "N" and height) or 1
         local target = nil
         for lookahead = lookstart, lookstop, step do
 
-            if map[lookahead][col] == nil then
-                if target == nil then 
-                    target = lookahead
-                end
+            if map[lookahead][col] == nil and target == nil then
+                target = lookahead
+
             elseif map[lookahead][col] == "#" then
                 target = nil
-                -- target = lookahead + step
-                -- if map[target][col] ~= nil and lookahead < height then
-                --     for i=1,height do
-                --         --try find empty spot
-                --         target = target + step
-                --         if target > height or map[target][col] == nil then
-                --             break
-                --         end
-                --     end
-                -- end
+                
             elseif map[lookahead][col] == "O" and target ~= nil then
                 map[target][col] = map[lookahead][col]
                 map[lookahead][col] = nil
                 target = target + step
             end
-            --if lookahead >= 10 then break end
         end
-        --if col >= 3 then break end
     end
 end
 
@@ -107,31 +64,18 @@ local function collapseEastWest(dir)
         local target = nil
         for lookahead = lookstart, lookstop, step do
 
-            if map[row][lookahead] == nil then
-                if target == nil then 
-                    target = lookahead
-                end
+            if map[row][lookahead] == nil and target == nil then
+                target = lookahead
+
             elseif map[row][lookahead] == "#" and lookahead < height then
                 target = nil
-                -- target = lookahead + step
-                -- if map[row][target] ~= nil and lookahead < height then
-                --     for i=1,height do
-                --         --try find empty spot
-                --         target = target + step
-                --         if target > height or map[row][target] == nil then
-                --             break
-                --         end
-                --     end
-                -- end
+
             elseif map[row][lookahead] == "O" and target ~= nil then
                 map[row][target] = map[row][lookahead]
                 map[row][lookahead] = nil
                 target = target + step
             end
-            --print('stat', lookahead, target)
-            --if lookahead >= 10 then break end
         end
-        --if col >= 3 then break end
     end
 end
 
@@ -156,34 +100,25 @@ local function cycle()
 end
 
 print(mapstring(map))
+
+local cache = {}
+local cyclelength
 for i=1,1000000000 do
-
     cycle()
-    if i%100000 == 0 then print(string.format("Cycle #%d P1 Score %d P2 Score %d", i, P1, scoremap())) end
-
+    local hash = md5.sumhexa(mapstring(map))
+    if cache[hash] ~= nil and 1000000000 % i == 0 then
+        cyclelength = i
+        break
+    else
+        cache[hash] = true
+    end
 end
+
 print(mapstring(map))
 
+print("cycle length is", cyclelength)
 P2 = scoremap()
-
-
 
 print('\nDay Fourteen')
 print(string.format('Part 1 - Answer %d',P1)) -- 105249
 print(string.format('Part 2 - Answer %d', P2)) -- 88680
-
---[[
-    P1 
-    6702153 too high
-
-    P2 
-    88772 too high, 100,000 cycles (0.01%)
-    88765 ?? 200,000 cycles
-    88711 ?? 300,000 cycles
-    88680 ?? 400,000 cycles (A Christmas fucking miracle)
-
-    I have a feeling it's another LCM ish trick
-    Track each bolder to see how long it takes to cycle
-    If cycle length is steady, position in future can be calculated
-
-]]
