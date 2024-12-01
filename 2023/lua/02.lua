b7e6458d7c1e3bd97d6978a30715a05e6189319a
Skyler Kehren <pyrodogg@gmail.com>
2024-12-01 18:55:27 +0200

    refactor 02

--- 2023/lua/02.lua
+++ 2023/lua/02.lua
@@ -1,76 +1,62 @@
 package.path = package.path .. ';../../?.lua'
 require "util"
-local lpeg = require "lpeg"
+local inspect = require "inspect"
 
-print('Day Two')
+local lines = lines_from('../input/02.txt')
+local P1, P2 = 0,0
+local maxDie = {red=12, green=13, blue=14}
 
-local file = '../input/02.txt'
-local lines = lines_from(file)
+local function validateGame(s)
 
-local accumulator = 0
-
-local maxDie = {}
-maxDie ["red"] = 12
-maxDie["green"] = 13
-maxDie["blue"] = 14
-
-function validateGame(s)
-
-    local valid_game = true
-
+    local gameID = tonumber(s:match('Game (%d+)'))
     local s = s .. ','
     for k, v in s:gmatch('(%d+) (%w+)[,;]') do
-        if (tonumber(k) > maxDie[v]) and maxDie[v] ~= nil then 
-            valid_game = false
-            break
+        if (tonumber(k) > maxDie[v]) and maxDie[v] ~= nil then
+            return 0
         end
-
     end
-
-    return valid_game
+    return gameID
 end
 
-for k,v in pairs(lines) do
+local function gamePower(s)
 
-    local gameID = tonumber(v:match('Game (%d+)'))
-
-    if validateGame(v) then 
-        accumulator = accumulator + gameID
-    end 
-
-end
-
---2685
-print(string.format('Part 1 - Answer %d', accumulator))
-
-
-function gamePower(s)
-
-    local minimums = {}
-
+    local minDie = {red=0,green=0,blue=0}
     local s = s .. ','
-    for k, v in s:gmatch('(%d+) (%w+)[,;]') do
+    for roll, color in s:gmatch('(%d+) (%w+)[,;]') do
 
-        if minimums[v] == nil then
-            minimums[v] = tonumber(k)
-        else
-            if tonumber(k) > minimums[v] then 
-                minimums[v] = tonumber(k)
-            end
-        end
+        minDie[color] = math.max(tonumber(roll,10), minDie[color])
     end
-
-    return minimums["red"] * minimums["green"] * minimums["blue"]
+    return reduce(minDie,function(a,v) return a*v end, 1)
 end
 
-accumulator = 0 -- reset accumulator
-for k,v in pairs(lines) do
+local function add(a,b) return a+b end
+local function mult(a,b) return a*b end
 
-    local score = gamePower(v)
+local function gamePower2(s)
+    local maxDice = reduce(unroll(s:gmatch('(%d+) (%w+)[,;]-')),
+    function(a,v)
+        -- v[roll, color]
+        a[v[2]] = math.max(tonumber(v[1],10),a[v[2]])
+        return a
+    end,
+    {red=0,green=0,blue=0})
 
-    accumulator = accumulator + score
-
+    return reduce(maxDice, mult, 1)
 end
 
---83707
-print(string.format('Part 2 - Answer %d\n', accumulator))
+-- local P3 = 0
+-- for _, v in pairs(lines) do
+--     P1 = P1 + validateGame(v)
+--     P2 = P2 + gamePower(v)
+--     P3 = P3 + gamePower2(v) -- reduce? ;)
+-- end
+
+P1, P2 = table.unpack(reduce(lines,
+function(a,v) 
+    return {a[1]+validateGame(v),a[2]+gamePower(v)}
+end, {0,0}))
+
+print('\nDay Two')
+print(string.format('Part 1 - Answer %s', P1)) --2685
+print(string.format('Part 2 - Answer %s', P2)) --83707
+--print(string.format('Part 2b - "Functional" %s', P3)) -- should match P2
